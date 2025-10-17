0dafbd6db3643c73aa005bfdbfd3b02b30025727
rosexalba <rosemaiellalv@gmail.com>
2025-10-16 18:10:56 -0700

    ternary login

--- Wave-5-JavaScript/JavaScript-curriculum/Project-05-Conditionals/04_ternary_login/script.js
+++ Wave-5-JavaScript/JavaScript-curriculum/Project-05-Conditionals/04_ternary_login/script.js
@@ -3,10 +3,29 @@
 // STEP 1: GET ELEMENTS
 // - input#ageInput, button#checkBtn, p#result
 
+const input = document.getElementById("ageInput");
+const button = document.getElementById("checkBtn");
+const p = document.getElementById("result");
+
 //
 // STEP 2: ADD CLICK LISTENER
 // - Convert input value to Number.
 // - Validate (Number.isNaN). If invalid, show a helpful message and stop.
+button.addEventListener("click", () => {
+    let age = input.valueAsNumber;
+
+    // validate input
+    let message = Number.isNaN(age) ? "Please provide a valid input" : "";
+    if(message) {
+        p.textContent = message;
+        return;
+    }
+     
+    // ternary check 
+    let label =(age >= 18) ? "Adult" : "Minor";
+    result.textContent = label;
+
+});
 
 //
 // STEP 3: TERNARY
