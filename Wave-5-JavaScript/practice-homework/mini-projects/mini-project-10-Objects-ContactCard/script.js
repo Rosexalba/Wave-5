6df598f878e798ebac2328735b0a793921b0a2ea
rosexalba <rosemaiellalv@gmail.com>
2025-10-22 18:48:29 -0700

    mini-project-10-objects

--- Wave-5-JavaScript/practice-homework/mini-projects/mini-project-10-Objects-ContactCard/script.js
+++ Wave-5-JavaScript/practice-homework/mini-projects/mini-project-10-Objects-ContactCard/script.js
@@ -68,20 +68,36 @@ STEP 3 — Write a helper function named showToast()
   - Action: unhides #status, then hides it after 2000ms
 */
 function showToast() {
-  
+
+  status.hidden = false;
+
+  setTimeout(() => {
+       status.hidden = true;
+  }, 2000);
+
 }
 /*
 STEP 4 — Write a helper function named removeEmptyState()
   - Function name: removeEmptyState
   - Action: if an element with class ".empty" exists inside #contacts, remove it
 */
-
+function removeEmptyState() {
+    const emptyMsg = contactsList.querySelector(".empty");   // contactlist refers to the container with the id = " contacts"
+                                                            // querySelector "empty": looks inside that container for any element with the class .empty 
+    if (emptyMsg) {                                        // if one exist it will be stored in the variable emptyMsg
+      emptyMsg.remove();     // Removes from the DOM if .empty 
+    }
+}
 /*
 STEP 5 — Write a helper function named restoreEmptyState()
   - Function name: restoreEmptyState
   - Action: replace the innerHTML of #contacts with the default empty-state block
 */
-
+function restoreEmptyState(){
+  contactsList.innerHTML = `
+  <div class ="empty">No Contacts yet - add your first one!</div>
+  `;
+}
 /*
 STEP 6 — Add a "submit" listener on form that runs an inline function
   - Read inputs into exact variable names:
@@ -90,8 +106,30 @@ STEP 6 — Add a "submit" listener on form that runs an inline function
   - Call removeEmptyState(), then append the new card to contactsList
   - Call showToast(), then reset the form
 */
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); // prevent page reload
 
+  const first = document.getElementById("firstName").value;
+  const last = document.getElementById("lastName").value;
+  const email = document.getElementById("email").value;
+  const phone = document.getElementById("phone").value;
+  const fav = document.getElementById("fav").checked;
+
+  const card = createContactCard({first,last,email,phone,fav});
+
+  removeEmptyState();
+
+  contactsList.appendChild(card);
+
+  showToast();         // shows "contact added"
+
+  form.reset();       // clears input fields 
+});
 /*
 STEP 7 — Add a "click" listener on clearBtn
   - When clicked, call restoreEmptyState()
 */
+
+clearBtn.addEventListener("click", () => {
+  restoreEmptyState();       // calls to Reset the contact list to default empty 
+});
